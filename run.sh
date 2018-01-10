bfd9e894e2f62b4b8192cac099c5dfe88e44fc98
Pawel <pawel905090@gmail.com>
2018-01-10 08:20:13 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ sudo rsync -a etc/apt/sources.list /etc/apt/
 sudo apt update && sudo apt upgrade -y
 
 ### installing packages
-sudo apt install -f -y i3 suckless-tools i3blocks xorg software-properties-common devscripts file-roller zsh pcmanfm leafpad eog pulseaudio pavucontrol alsa-utils gdebi numix-gtk-theme numix-icon-theme screenfetch feh conky build-essential gtk2-engines-murrine gtk2-engines vim ranger caca-utils highlight atool w3m poppler-utils mediainfo compton gparted git python-pip libcanberra-gtk-module
+sudo apt install -f -y i3 suckless-tools i3blocks xorg software-properties-common devscripts snapd file-roller zsh pcmanfm leafpad eog pulseaudio pavucontrol alsa-utils gdebi numix-gtk-theme numix-icon-theme screenfetch feh conky build-essential gtk2-engines-murrine gtk2-engines vim ranger caca-utils highlight atool w3m poppler-utils mediainfo compton gparted git python-pip libcanberra-gtk-module
 
 ### creating dirs
 mkdir ~/.config/
